f6160fad93046dc841d7e68252be20b5f5d366a8
rafaexperience <ereemece@gmail.com>
2015-07-29 04:05:48 +0200

    Primera pregunta.

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+﻿// GET /quizes/question
+//exporta la funcion como quiz_controller.question
+exports.question = function (req, res) {
+    //monta la pagina quizes/question con la pregunta como objeto 
+    res.render("quizes/question", { pregunta: "Capital de Italia?: " });
+};
+// GET /quizes/answer
+//exporta la funcion como quiz_controller.answer
+exports.answer = function (req, res) {
+    if (req.query.respuesta === "Roma") {
+        //monta la pagina quizes/answer y le envia respuesta con el valor correspondiente 
+        res.render("quizes/answer", { respuesta: "¡¡ Asin es !!" });
+    } else {
+        res.render("quizes/answer", { respuesta: "Que dices, tarao..." });
+    }
+    
+};
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var express = require('express');
 //asigna el comportamiento del objeto Router() de express a router
 var router = express.Router();
 
+//asignamos a variable quizController las funciones exportadas en archivo /controllers/quiz_controller.
+//  .question y .answer
+var quizController = require("../controllers/quiz_controller");
+
 // acepta peticion GET / o /index y envia la pagina index.js con la variable title con valor "Express"
 router.get('/', function (req, res) {
     //res.render(vista, params)-->respuesta.generaPagina(pagina.ejs,objeto-
     //con parametros (variables en ejs=<%=var%>)
   res.render('index', { title: 'rafaQuiz' });
 });
+
+// Enviamos las peticiones get de quizes/.. a quizController --> controllers/quiz_controller.js
+router.get("/quizes/question", quizController.question);
+router.get("/quizes/answer", quizController.answer);
 
 module.exports = router; // hace que router se pueda exportar a otros archivos
