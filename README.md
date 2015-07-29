ab64b9b60965b79e8663f68ccd08aae8dba12c9b
rafaexperience <ereemece@gmail.com>
2015-07-29 18:01:54 +0200

    CSS adaptable a móviles y PC´s

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 Ejercicio Miriadax Curso 2
 Desarrollo de servicios en la nube con HTML5, Javascript y node.js
 
-Módulo 6 en adelante.
+M&#243;dulo 6 en adelante.
 
 licencia:MIT
+
 Despliegue HEROKU: https://rafaquiz.herokuapp.com/
--(npm install para añadir carpeta node_modules) se instalaran los modulos segun package.json
+- ver notas.txt para historial detallado de la aplicaci&#243;n
+
+-(npm install para a&#241;adir carpeta node_modules) se instalar&#225;n los m&#243;dulos segun package.json
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,16 +2,16 @@
 //exporta la funcion como quiz_controller.question
 exports.question = function (req, res) {
     //monta la pagina quizes/question con la pregunta como objeto 
-    res.render("quizes/question", { pregunta: "Capital de Italia?: " });
+    res.render("quizes/question", { pregunta: "Capital de Italia ?: ", title: "Pregunta rafaQuiz" });
 };
 // GET /quizes/answer
 //exporta la funcion como quiz_controller.answer
 exports.answer = function (req, res) {
     if (req.query.respuesta === "Roma") {
         //monta la pagina quizes/answer y le envia respuesta con el valor correspondiente 
-        res.render("quizes/answer", { respuesta: "¡¡ Asin es !!" });
+        res.render("quizes/answer", { respuesta: "¡¡ Asin es !!", title: 'Respuesta rafaQuiz' });
     } else {
-        res.render("quizes/answer", { respuesta: "Que dices, tarao..." });
+        res.render("quizes/answer", { respuesta: "Que dices, tarao...", title: "Respuesta rafaQuiz" });
     }
     
 };
