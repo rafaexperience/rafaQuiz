9b74d28058a3c3e6e2ca9a1f7a2fe7b6f3598a48
rafaLinux <erreemece@live.com>
2015-08-01 00:05:03 +0200

    quiz 9. Multiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,25 +2,38 @@
 var models = require("../models/models.js");
 
 
-// GET /quizes/question
-//exporta la funcion como quiz_controller.question
-exports.question = function (req, res) {
-    //coge todos los datos de quiz.sqlite y los pasa como parametro array quiz[]
-    models.Quiz.findAll().then(function (quiz) {
-        //monta la pagina quizes/question con la pregunta como objeto 
-        res.render("quizes/question", { pregunta: quiz[0].pregunta, title: "Pregunta rafaQuiz" });
+// GET /quizes/index
+//exporta la funcion como quiz_controller.index
+exports.index = function (req, res) {
+    //coge todos los datos de bd y los pasa como parametro array quizes
+    models.Quiz.findAll().then(function (quizes) {
+        //monta la pagina quizes/index con param quizes (obj con todas preguntas) 
+        res.render("quizes/index", { quizes: quizes, title: "Preguntas rafaQuiz" });
     })
 };
-// GET /quizes/answer
+
+//GET /quizes/:id
+exports.show =function(req, res){
+//exporta la funcion como quiz_controller.show
+    models.Quiz.find(req.params.quizId).then(function(quiz){
+        //coge el parametro id y lo usa para buscar un quiz :id-->quizId
+        //Muestra el la pagina de la pregunta con el quiz pasado
+        res.render("quizes/show", { quiz: quiz, title: "Pregunta rafaQuiz" });
+    })
+};
+
+// GET /quizes/:id/answer
 //exporta la funcion como quiz_controller.answer
 exports.answer = function (req, res) {
-    //coge todos los datos de quiz.sqlite y los pasa como parametro array quiz[]
-    models.Quiz.findAll().then(function (quiz) {
-        if (req.query.respuesta === quiz[0].respuesta) {
+    //busca pregunta segun parametro :id y pasa el objeto como quiz
+    models.Quiz.find(req.params.quizId).then(function (quiz) {
+        if (req.query.respuesta === quiz.respuesta) {
             //monta la pagina quizes/answer y le envia respuesta con el valor correspondiente 
-            res.render("quizes/answer", { respuesta: "¡¡ Asin es !!", title: 'Respuesta rafaQuiz' });
+            res.render("quizes/answer", { quiz:quiz, respuesta: "¡¡ Asin es !!",
+             title: 'Respuesta rafaQuiz' });
         } else {
-            res.render("quizes/answer", { respuesta: "Que dices, tarao...", title: "Respuesta rafaQuiz" });
+            res.render("quizes/answer", { quiz:quiz, respuesta: "Que dices, tarao...",
+             title: "Respuesta rafaQuiz" });
         }
     })
 };
--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,12 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'rafaQuiz' });
 });
 
+// -----------DEFINICION DE RUTAS DE QUIZES------------------
 //GET /quizes/..
 // Enviamos las peticiones get de /quizes/.. a quizController --> controllers/quiz_controller.js
-router.get("/quizes/question", quizController.question);
-router.get("/quizes/answer", quizController.answer);
+router.get("/quizes", quizController.index);
+router.get("/quizes/:quizId(\\d+)", quizController.show)
+router.get("/quizes/:quizId(\\d+)/answer", quizController.answer);
 
 //GET /author
 // Enviamos peticiones get de /author a quizController
